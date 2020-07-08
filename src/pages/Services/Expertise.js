ac6411fdb8ccf35d1895f21544462c4e94baeaeb
Aniketh <anikethnair97@gmail.com>
2020-07-09 05:25:24 +0530

    Partners Section

--- src/pages/Services/Expertise.js
+++ src/pages/Services/Expertise.js
@@ -3,30 +3,10 @@ import { Row, Col, Card } from "antd";
 
 const services = [
   {
-    heading: "Management Systems",
-    md: 12,
-    lg: 8,
-    cardStyles: "white-card",
-    subServices: [
-      {
-        name: "CMS",
-        description: "",
-      },
-      {
-        name: "CRM",
-        description: "",
-      },
-      {
-        name: "LMS",
-        description: "",
-      },
-    ],
-  },
-  {
     heading: "The Virtual Store",
     md: 24,
     lg: 24,
-    cardStyles: "white-card",
+    cardStyles: "white-card eCommerce",
     subServices: [
       {
         name: "E - Commerce",
@@ -38,11 +18,12 @@ const services = [
     heading: "Online Business Cards",
     md: 12,
     lg: 12,
-    cardStyles: "white-card",
+    cardStyles: "white-card bCards",
     subServices: [
       {
         name: "Portfolio",
         description: "",
+        cardStyle: "rightAlign"
       },
       {
         name: "Company Website",
@@ -54,15 +35,36 @@ const services = [
     heading: "App Development",
     md: 12,
     lg: 12,
-    cardStyles: "blue-card",
+    cardStyles: "blue-card apps",
     subServices: [
       {
         name: "Hybrid App",
         description: "",
+        cardStyle: "rightAlign"
       },
       {
         name: "Native App",
         description: "",
+      },
+    ],
+  },
+  {
+    heading: "Management Systems",
+    md: 12,
+    lg: 8,
+    cardStyles: "white-card",
+    subServices: [
+      {
+        name: "CMS",
+        description: <p>Equip yourself with a <b>Content Management System</b> flexible enough to use as a personal blog or even a channel serving multi-format content to a large audience.</p>,
+      },
+      {
+        name: "CRM",
+        description: <p>For your growing clientele & expanding business <b>Customer Relationship Management</b> is vital. Sprinkle in some analytics that matters to your business and you have to perfext tool meet those annual sales targets</p>,
+      },
+      {
+        name: "LMS",
+        description: <p>With the dawn of the next industrial revolution, education is no longer limited to the boudaries of traditional University. Are you looking to build a <b>Learning Management System</b>? Hop on! We'll ride this wave of online learning together.</p>,
       },
     ],
   },
@@ -74,7 +76,7 @@ export default function Expertise() {
         <div key={service.heading} className="service-block">
           <h2 className="service-heading">{service.heading}</h2>
           <div className="service-card-container">
-            <Row className="service-row" gutter={8}>
+            <Row className="service-row" justify="center" gutter={8}>
               {service.subServices.map((subService) => (
                 <Col
                   key={subService.name}
@@ -83,11 +85,11 @@ export default function Expertise() {
                   md={service.md}
                   lg={service.lg}
                 >
-                  <Card className={`service-card ${service.cardStyles}`}>
+                  <Card className={`service-card ${service.cardStyles} ${subService.cardStyle}`}>
                     <h4 className="service-name">{subService.name}</h4>
-                    <p className="service-description">
+                    <div className="service-description">
                       {subService.description}
-                    </p>
+                    </div>
                   </Card>
                 </Col>
               ))}
--- src/pages/Services/Partners.js
+++ src/pages/Services/Partners.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+
+// Images
+import odl from "static/images/media/partners/odl.png";
+import gcs from "static/images/media/partners/gcs.png";
+import mwm from "static/images/media/partners/mwm.png";
 
 export default function Partners() {
-    return (
-        <div className="partners">
-            <h2 className="heading">Our Partners</h2>
+  return (
+    <div className="partners">
+      <h2 className="heading">Our Partners</h2>
+      <div className="images-container">
+        <div className="image-container">
+          <a href="http://omegadigitallabs.com/" rel="noopener noreferrer" target="_blank" className="partner-link">
+            <img className="partner-image" src={odl} alt="Omega Digital Labs" />
+          </a>
         </div>
-    )
+        <div className="image-container">
+          <a href="https://giriconsultancy.in/" rel="noopener noreferrer" target="_blank" className="partner-link">
+            <img
+              className="partner-image"
+              src={gcs}
+              alt="Giri Consultancy Services"
+            />
+          </a>
+        </div>
+        <div className="image-container">
+          <a href="https://www.marketwithmaddy.com/" rel="noopener noreferrer" target="_blank" className="partner-link">
+            <img className="partner-image" src={mwm} alt="Market with Maddy" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
