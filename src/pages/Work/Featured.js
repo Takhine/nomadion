dc05fdf7873154511d2fa6d39f122d623ed89ebc
Aniketh <anikethnair97@gmail.com>
2020-07-12 02:49:08 +0530

    Projects

--- src/pages/Work/Featured.js
+++ src/pages/Work/Featured.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import { Affix, Menu, Tag, Card, Row, Col, Button } from "antd";
+import { Affix, Menu, Tag, Card, Row, Col, Button, Divider } from "antd";
 import Fade from "react-reveal/Fade";
-import people from 'static/images/icons/people-icon.svg';
-import peopleBlue from 'static/images/icons/people-blue.svg';
-
+import people from "static/images/icons/people-icon.svg";
+import peopleBlue from "static/images/icons/people-blue.svg";
+import eventgrab from "static/images/media/projects/eventgrab.png";
+import grassbox from "static/images/media/projects/grassbox.jpg";
+import copia from "static/images/media/projects/copia.jpg";
 
 const StartupsContent = () => {
   return (
@@ -11,6 +13,55 @@ const StartupsContent = () => {
       <div className="tags-row">
         <Tag className="blue-tag tag">Eventgrab</Tag>
         <Tag className="blue-tag tag">Grassbox</Tag>
+      </div>
+      <div className="startups-container">
+        <Row gutter={16} className="startup-row">
+          <Col xs={24} md={12}>
+            <Card className="project-card startup-card eventgrab">
+              <img src={eventgrab} alt="Eventgrab Icon" />
+            </Card>
+          </Col>
+          <Col xs={24} md={12} className="startup-content">
+            <h4 className="project-name">Eventgrab</h4>
+            <Fade>
+              <div className="project-description">
+                Here at Eventgrab, we are devoted to ensuring that any event you
+                plan is a triumphant success. This project started out as an
+                elementary collaboration between event hosts, us, and equipment
+                vendors. However, it has fast become a complex and
+                interconnected network of event hosting personnel, now equipped
+                with an online platform to easily facilitate the renting and
+                managing of event equipment that ranges from smaller
+                gadgets/appliances like speakers and microphones to heavy duty
+                equipment like furniture and infrastructure.
+              </div>
+            </Fade>
+            <Button className="project-button">Visit Website</Button>
+          </Col>
+        </Row>
+        <Divider style={{borderTop: '1px solid #00A8E8'}} />
+        <Row gutter={16} className="startup-row">
+          <Col xs={24} md={12}>
+            <Card className="project-card startup-card grassbox">
+              <img src={grassbox} alt="Grassbox Icon" />
+            </Card>
+          </Col>
+          <Col xs={24} md={12} className="startup-content">
+            <h4 className="project-name">Grassbox</h4>
+            <Fade>
+              <div className="project-description">
+                Grassbox is a project run by avid football enthusiasts with an
+                aim to cultivate and foster Indian football. We are a hub for
+                football tournaments and turf games throughout the city of
+                Mumbai, connecting teams to leagues across the country and
+                developing the football base in India.
+              </div>
+            </Fade>
+            <Button className="project-button">
+              <img src={people} alt="People Icon" /> Learn More
+            </Button>
+          </Col>
+        </Row>
       </div>
     </div>
   );
@@ -28,7 +79,11 @@ const InitiativesContent = () => {
         <Col xs={24} md={12} className="initiative-content">
           <h4 className="project-name">NGO/NPO Platform</h4>
           <Fade>
-            <div className="project-description">You care about giving back to the world? We're really interested in working with you on our upcoming project. <b style={{color:'#00A8E8'}}>Let's Connect</b>!</div>
+            <div className="project-description">
+              You care about giving back to the world? We're really interested
+              in working with you on our upcoming project.{" "}
+              <b style={{ color: "#00A8E8" }}>Let's Connect</b>!
+            </div>
           </Fade>
           <Button className="project-button">
             <img src={people} alt="People Icon" /> Learn More
@@ -40,7 +95,30 @@ const InitiativesContent = () => {
 };
 
 const ProjectsContent = () => {
-  return <div></div>;
+  return (
+    <div className="live-container">
+      <Row gutter={16} className="live-row">
+        <Col xs={24} md={12}>
+          <Card className="project-card live-card">
+            <img src={copia} alt="Copia Icon" />
+          </Card>
+        </Col>
+        <Col xs={24} md={12} className="live-content">
+          <h4 className="project-name">Copia</h4>
+          <Fade>
+            <div className="project-description">
+              You care about giving back to the world? We're really interested
+              in working with you on our upcoming project.{" "}
+              <b style={{ color: "#00A8E8" }}>Let's Connect</b>!
+            </div>
+          </Fade>
+          <Button className="project-button">
+            <img src={people} alt="People Icon" /> Learn More
+          </Button>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 export default function Featured() {
   const [current, setCurrent] = useState("initiative");
@@ -49,7 +127,7 @@ export default function Featured() {
     setCurrent(e.key);
   };
   return (
-    <div className="featured" style={{ height: "1800px" }}>
+    <div className="featured">
       <Affix offsetTop={40} className="projects-navigation">
         <Menu
           className="projects-menu"
